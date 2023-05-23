f7b58f8ed2c1da8a8d6013ce226d14a41d41b247
Aldoleonel <aldoleonelchalup@gmail.com>
2023-05-23 13:06:28 -0300

    agrega instrucciones

--- ensaladera.js
+++ ensaladera.js
@@ -1,1 +1,8 @@
 console.log("aqui esta ensaladera");
+console.log(`
+INSTRUCCIONES
+1. lavar las verduras
+2. cortar la lechuga con cuchillo
+3. pelar el tomate
+4. cortar la cebolla en rodajas
+`);
