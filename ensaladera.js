c335ade61ce54bdd11a61726e0d7ddd36586e84a
Aldoleonel <97469028+Aldoleonel@users.noreply.github.com>
2023-05-23 13:05:49 -0300

    agrega instrucciones

--- ensaladera.js
+++ ensaladera.js
@@ -1,1 +1,8 @@
 console.log("aqui esta ensaladera");
+console.log(`
+INSTRUCCIONES
+1. lavar las verduras
+2. cortar la lechuga con la mano
+3. cortar el tomate en cubo
+4. cortar la cebolla en juliana
+            
